5ac1bc5a6e57e5c34bc72b689383ca20122797b4
Angel Aguayo <jose.aguayo4679@alumnos.udg.mx>
2024-10-20 12:30:51 -0600

    Primer Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Componente from './component';
+import Navbar from './navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navbar />
+      <header className="text-center mt-5">
+        <h1>Bienvenido a Eventia</h1>
+        <p className="lead">Explora eventos socioculturales cerca de ti.</p>
       </header>
     </div>
   );
