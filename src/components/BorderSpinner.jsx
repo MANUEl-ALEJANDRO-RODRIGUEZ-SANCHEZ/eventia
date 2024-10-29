5d7af4767d85c5824f8b03b99a538df8bf41b730
Manuel Alejandro Rodriguez Sanchez <manuel.rodriguez9120@alumnos.udg.mx>
2024-10-28 23:29:14 -0600

    Delete src directory

--- src/components/BorderSpinner.jsx
+++
@@ -1,9 +0,0 @@
-function BorderSpinner() {
-  return (
-    <div className="spinner-border text-secondary" role="status">
-        <span className="visually-hidden">Cargando...</span>
-    </div>
-  )
-}
-
-export default BorderSpinner;
--- src/middleware.js
+++
@@ -1,5 +0,0 @@
-export { default } from "next-auth/middleware";
-
-// routes to protect from login
-// export const config = { matcher: ["/perfil", "/eventos/"]};
-export const config = { matcher: ["/perfil", "/eventos/:path*", "/api/:path*"] };
