e38aa6c2e5e8de087df0495af1eb7ef1d1d9c6ea
Manuel Alejandro Rodriguez Sanchez <manuel.rodriguez9120@alumnos.udg.mx>
2024-10-28 21:30:28 -0600

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,10 +1,19 @@
+// import { createPool } from "mysql2/promise";
+
+// const conn = createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE
+// });
+
+// export {conn};
+
+
 import { createPool } from "mysql2/promise";
 
 const conn = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    uri: process.env.DB_URL
 });
 
-export {conn};
+export { conn };
